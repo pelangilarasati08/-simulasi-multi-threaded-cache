b4d1ee970c2d5cfae8673c1110abaa339e1acc0d
pelangilarasati08 <pelangilarasati008@gmail.com>
2025-05-12 10:20:54 +0700

    Add files via upload

---
+++ cache_simulation_result_example.py
@@ -0,0 +1,3 @@
+# Simulasi perbandingan cache dengan dan tanpa protokol koherensi (MESI)
+result_none = {'protocol': 'none', 'execution_time': 0.2960517406463623, 'metrics': {'cache_misses': 48, 'coherence_messages': 0, 'memory_accesses': 1600}}
+result_mesi = {'protocol': 'MESI', 'execution_time': 0.2978501319885254, 'metrics': {'cache_misses': 791, 'coherence_messages': 1946, 'memory_accesses': 1600}}
